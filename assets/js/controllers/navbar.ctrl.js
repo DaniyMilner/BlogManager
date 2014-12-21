a2b8d811636d307f8cf11366acb2682c72bc4c0e
Aleksey Olenyuk <alo@qapint.com>
2014-12-21 13:05:32 +0200

    format code

--- assets/js/controllers/navbar.ctrl.js
+++ assets/js/controllers/navbar.ctrl.js
@@ -1,24 +1,24 @@
 "use strict";
 
 angular
-  .module('blogApp')
-  .controller('navbarCtrl',
-  ['$scope', '$sails', function($scope, $sails){
-    $scope.createUser = function(){
-      $sails.post('/create', {
-        'email':'alo@qapint.com',
-        'password': '111111'
-      }).success(function(data){
-        console.log(data);
-      });
-    };
-    $scope.login = function(){
-      $sails.post('/login', {
-        'email':'alo@qapint.com',
-        'password': '111111'
-      }).success(function(data){
-        console.log(data);
-      });
-    };
-  }]
-);
+	.module('blogApp')
+	.controller('navbarCtrl',
+		['$scope', '$sails', function($scope, $sails){
+			$scope.createUser = function(){
+				$sails.post('/create', {
+					'email': 'alo@qapint.com',
+					'password': '111111'
+				}).success(function(data){
+					console.log(data);
+				});
+			};
+			$scope.login = function(){
+				$sails.post('/login', {
+					'email': 'alo@qapint.com',
+					'password': '111111'
+				}).success(function(data){
+					console.log(data);
+				});
+			};
+		}]
+	);
--- assets/js/module.js
+++ assets/js/module.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('blogApp', [
-  'ui.router',
-  'ngSails'
-])
-  .run(
-  ['$rootScope', '$state', '$stateParams',
-    function($rootScope, $state, $stateParams){
-      $rootScope.$state = $state;
-      $rootScope.$stateParams = $stateParams;
-    }]);
+		'ui.router',
+		'ngSails'
+	])
+	.run(
+		['$rootScope', '$state', '$stateParams',
+			function($rootScope, $state, $stateParams){
+				$rootScope.$state = $state;
+				$rootScope.$stateParams = $stateParams;
+			}]);
